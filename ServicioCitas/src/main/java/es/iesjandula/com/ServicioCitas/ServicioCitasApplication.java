9f30ec7c818a30dc0e36b9614a5b85cd09c5e604
Jorge-W3 <jorgeortegazbbto@gmail.com>
2024-11-04 16:23:51 +0100

    Inicio

---
+++ ServicioCitas/src/main/java/es/iesjandula/com/ServicioCitas/ServicioCitasApplication.java
@@ -0,0 +1,13 @@
+package es.iesjandula.com.ServicioCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCitasApplication.class, args);
+	}
+
+}
