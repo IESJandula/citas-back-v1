391962511e884d985a955ca2cb5b4d1863e25ee6
Jorge-W3 <jorgeortegazbbto@gmail.com>
2024-11-08 18:16:01 +0100

    Servicios CRUD de Horarios

--- ServicioCitas/src/main/java/es/iesjandula/com/ServicioCitas/Repositorios/RepositorioHorarios.java
+++ ServicioCitas/src/main/java/es/iesjandula/com/ServicioCitas/Repositorios/RepositorioHorarios.java
@@ -1,4 +1,9 @@
 package es.iesjandula.com.ServicioCitas.Repositorios;
 
-public class RepositorioHorarios {
+import es.iesjandula.com.ServicioCitas.Entidades.Horarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioHorarios extends JpaRepository<Horarios, Long> {
 }
--- ServicioCitas/src/main/java/es/iesjandula/com/ServicioCitas/Servicios/ServicioHorarios.java
+++ ServicioCitas/src/main/java/es/iesjandula/com/ServicioCitas/Servicios/ServicioHorarios.java
@@ -1,4 +1,50 @@
 package es.iesjandula.com.ServicioCitas.Servicios;
 
+import es.iesjandula.com.ServicioCitas.Entidades.Horarios;
+import es.iesjandula.com.ServicioCitas.Repositorios.RepositorioHorarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ServicioHorarios {
+
+    @Autowired
+    private RepositorioHorarios repositorioHorarios;
+
+    //Metodos CRUD
+
+    public Horarios añadirHorario(Horarios horario) {
+        return repositorioHorarios.save(horario);
+
+    }
+    public List<Horarios> listarHorarios() {
+        return repositorioHorarios.findAll();
+    }
+
+    public Optional<Horarios> buscarHorario(Long id) {
+        return repositorioHorarios.findById(id);
+    }
+
+    public Horarios cambiarHorario(Long id, Horarios horarioCambiado) {
+        Horarios horario = repositorioHorarios.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el horario"));
+
+        horario.setDiaSemana(horarioCambiado.getDiaSemana());
+        horario.setHoraInicio(horarioCambiado.getHoraInicio());
+        horario.setHoraFin(horarioCambiado.getHoraFin());
+
+        return repositorioHorarios.save(horario);
+    }
+    public void eliminarHorario(Long id) {
+       Horarios horaio = repositorioHorarios.findById(id)
+               .orElseThrow(() -> new RuntimeException("No existe el horario"));
+       repositorioHorarios.delete(horaio);
+    }
+
+
 }
+
+
